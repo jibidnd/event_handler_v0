3fbaaab52c4e79efa40b6427a122b548f7b35dc7
jibi <robert05101@gmail.com>
2020-09-16 18:43:15 -0700

    Added custom snowflake feed for backtrader, and added unix2num function

--- data/utils/utils.py
+++ data/utils/utils.py
@@ -1,18 +1,37 @@
 import datetime
+import math
+import pytz
+
+#======================================================================================================================
+# Useful constants
+#======================================================================================================================
+HOURS_PER_DAY = 24.0
+MINUTES_PER_HOUR = 60.0
+SECONDS_PER_MINUTE = 60.0
+MUSECONDS_PER_SECOND = 1e6
+NANOSECONDS_PER_SECOND = 1e9
+MINUTES_PER_DAY = MINUTES_PER_HOUR * HOURS_PER_DAY
+SECONDS_PER_DAY = SECONDS_PER_MINUTE * MINUTES_PER_DAY
+MUSECONDS_PER_DAY = MUSECONDS_PER_SECOND * SECONDS_PER_DAY
+NANOSECONDS_PER_DAY = NANOSECONDS_PER_SECOND * SECONDS_PER_DAY
+
+
+
+
 
 def generate_days(start, end, step_in_days):
-	'''
-	start and end can be either datetime.datetime instances or string of yyyy-mm-dd format.
-	Retunrs list of dates as datetime.datetime instances
-	'''
-	if isinstance(start, str):
-		start = datetime.datetime.strptime(start, '%Y-%m-%d')
-	if isinstance(end, str):
-		end = datetime.datetime.strptime(end, '%Y-%m-%d')
+    '''
+    start and end can be either datetime.datetime instances or string of yyyy-mm-dd format.
+    Retunrs list of dates as datetime.datetime instances
+    '''
+    if isinstance(start, str):
+        start = datetime.datetime.strptime(start, '%Y-%m-%d')
+    if isinstance(end, str):
+        end = datetime.datetime.strptime(end, '%Y-%m-%d')
 
-	dates = [(start + datetime.timedelta(days = step_in_days * i)) for i in range((end - start).days + 1)]
+    dates = [(start + datetime.timedelta(days = step_in_days * i)) for i in range((end - start).days + 1)]
 
-	return dates
+    return dates
 
 def to_columnar(lst_dicts, data_keys) -> dict:
     '''
@@ -25,3 +44,47 @@ def to_columnar(lst_dicts, data_keys) -> dict:
     :param list data_keys: a list of column names to extract from the rows
     '''
     return {k: [dict_i.get(k) for dict_i in lst_dicts] for k in data_keys}
+
+def unix2num(unix_timestamp, tz = pytz.timezone('UTC'), resolution = 'milliseconds'):
+    """
+    Convert a unix timestamp (nanosecond resolution) to the Gregorian
+    date as UTC float days, preserving hours, minutes, seconds, microseconds,
+    and nanoseconds.
+    Return value is a :func:`float`.
+    """
+    # First convert to second resolution and get nanosecond portion, if any
+    nanoseconds = 0
+    if resolution == 'seconds':
+        pass
+    elif resolution == 'milliseconds':
+        unix_timestamp *= 1e-3
+    elif resolution == 'microseconds':
+        unix_timestamp *= 1e-6
+    elif resolution == 'nanoseconds':
+        nanoseconds = unix_timestamp % 1e6
+        unix_timestamp *= 1e-9
+
+    # convert to datetime.datetime instance
+    # This gets us to microsecond resolution
+    dt = datetime.datetime.utcfromtimestamp(unix_timestamp)
+
+    if tz is not None:
+        dt = tz.localize(dt)
+
+    if hasattr(dt, 'tzinfo') and dt.tzinfo is not None:
+        delta = dt.tzinfo.utcoffset(dt)
+        if delta is not None:
+            dt -= delta
+
+    base = float(dt.toordinal())
+    if hasattr(dt, 'hour'):
+        base = math.fsum((
+            base,
+            dt.hour / HOURS_PER_DAY,
+            dt.minute / MINUTES_PER_DAY,
+            dt.second / SECONDS_PER_DAY,
+            dt.microsecond / MUSECONDS_PER_DAY,
+            nanoseconds / NANOSECONDS_PER_DAY 
+            ))
+
+    return base
